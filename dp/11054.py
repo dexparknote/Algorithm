874ef5268fac761bf93ce8c9a66cd9f12d8a93a1
dexpark <mns0327@naver.com>
2021-03-15 12:41:36 +0900

    조금만 변경했더니 정답! 사실 이전 코딩 답은 맞는데 어떤 테스트케이스에서 틀렸는지 잘 모르겠음

--- dp/11054.py
+++ dp/11054.py
@@ -3,21 +3,25 @@
 # 예제) 1 5 2 1 4 3 4 5 2 1 -> 1 2 3 4 5 2 1 길이는 7
 n = int(input())
 a = list(map(int,input().split()))
-dp = [1] * n
+dp1 = [1] * n
 dp2 = [1] * n
 result = [1] * n
 for i in range(n):
     for j in range(i):
         if a[i] > a[j]:
-            dp[i] = max(dp[i], dp[j]+1)
+            dp1[i] = max(dp1[i], dp1[j]+1)
 
-for i in range(n-1,0,-1):
-    for j in range(n-1,0,-1):
+a.reverse()
+
+for i in range(n):
+    for j in range(i):
         if a[i] > a[j]:
             dp2[i] = max(dp2[i], dp2[j]+1)
 
-for i in range(n):
-    result[i] = dp[i] + dp2[i] -1
+dp2.reverse()
 
-print(max(result))
+for i in range(n):
+    result[i] = dp1[i] + dp2[i]
+
+print(max(result)-1)
 
