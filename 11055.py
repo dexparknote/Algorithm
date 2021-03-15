6afb4973f13c12abd163c55721b7645958f10523
dexpark <mns0327@naver.com>
2021-03-15 11:56:05 +0900

    로직은 맞는거 같았으나 dp 리스트 초기화가 문제였음 :)

--- 11055.py
+++ 11055.py
@@ -2,12 +2,10 @@
 # 합이 가장 큰 증가 부분 수열은 1,2,50,60 이고, 합은 113이다.
 n = int(input())
 a = list(map(int,input().split()))
-dp = [1 for _ in range(n)]
-dp[0] = a[0]
+dp = [i for i in a] #초기화는 입력받은 값으로 로직은 맞았으나 리스트 초기화 문제였음.
 for i in range(n):
     for j in range(i):
         if a[i] > a[j]:
             dp[i] = max(dp[i], dp[j]+a[i])
-print(dp)
-
+print(max(dp))
 
